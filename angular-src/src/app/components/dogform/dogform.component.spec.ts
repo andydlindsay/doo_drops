468477f387a209f4e7d77f0a5486cc0da1c85177
andydlindsay <andydlindsay@gmail.com>
2017-04-23 16:27:05 -0700

    removes boilerplate tests form components

--- angular-src/src/app/components/dogform/dogform.component.spec.ts
+++ angular-src/src/app/components/dogform/dogform.component.spec.ts
@@ -2,24 +2,24 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DogformComponent } from './dogform.component';
 
-describe('DogformComponent', () => {
-  let component: DogformComponent;
-  let fixture: ComponentFixture<DogformComponent>;
+// describe('DogformComponent', () => {
+//   let component: DogformComponent;
+//   let fixture: ComponentFixture<DogformComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DogformComponent ]
-    })
-    .compileComponents();
-  }));
+//   beforeEach(async(() => {
+//     TestBed.configureTestingModule({
+//       declarations: [ DogformComponent ]
+//     })
+//     .compileComponents();
+//   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DogformComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+//   beforeEach(() => {
+//     fixture = TestBed.createComponent(DogformComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
